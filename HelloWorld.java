43129c89ee2bf27b127cd6bd5af0d6cd7874bb60
frndzfreek <madhu.mithaemails@gmail.com>
2025-01-23 20:01:19 +0530

    P1-Basic

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
+//Java Basics
+//Aim:To print Hello World in Java
+
+//The class name should be kept the same as the file name
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World");
