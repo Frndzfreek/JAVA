aaa6c137f2f9f951be0600b3e23a3c2f6b4b0fa7
frndzfreek <madhu.mithaemails@gmail.com>
2025-01-31 19:49:53 +0530

    Arrays

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays{
+    public static void main(String args[]){
+        int A[] = {1,2,3,4,5}; //1-D Array
+        for (int num : A) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
