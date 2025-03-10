0b2035e2d132d3c88dea25135fbd4fcd54a5348c
frndzfreek <madhu.mithaemails@gmail.com>
2025-03-10 11:54:54 +0530

    update

---
+++ LOOPs/AscendingOrder.java
@@ -0,0 +1,32 @@
+package LOOPS;
+
+import java.util.Scanner;
+
+class AscendingOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        if (b > c) {
+            int temp = b;
+            b = c;
+            c = temp;
+        }
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        System.out.println("Ascending Order: " + a + ", " + b + ", " + c);
+        scanner.close();
+    }
+}
