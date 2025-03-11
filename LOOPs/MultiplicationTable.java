4ae38c4f750d9aa7606039a260041f7eb0f64a30
Madhumitha <madhu.mithaemails@gmail.com>
2025-03-11 19:49:23 +0530

    Update MultiplicationTable.java

--- LOOPs/MultiplicationTable.java
+++ LOOPs/MultiplicationTable.java
@@ -1,11 +1,12 @@
 package LOOPS;
 
-class F_MultiplicationTable {
+class MultiplicationTable {
     public static void main(String[] args){
         //int n = 2;
         // for (int i =1;i<=10;i++){
         //     System.out.println(n+"x"+i+"="+n*i);
         // }
+        //or
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
                 System.out.print(i * j + "\t");
