e55287bf9f444c1b0c5acbfb90c905355d7ec01a
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-01 16:54:04 +0530

    Create RotateMatrix90.java

---
+++ ARRAYs/RotateMatrix90.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class RotateMatrix90{
+    public static void rotateMatrix90(int[][] matrix) {
+        int n = matrix.length;
+        
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0, k = n - 1; j < k; j++, k--) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+            }
+        }
+    }
+    
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int elem : row) {
+                System.out.print(elem + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the size of the matrix (n x n): ");
+        int n = scanner.nextInt();
+        int[][] matrix = new int[n][n];
+        
+        System.out.println("Enter the matrix elements:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+        
+        rotateMatrix90(matrix);
+        
+        System.out.println("Rotated Matrix:");
+        printMatrix(matrix);
+        
+        scanner.close();
+    }
+}
