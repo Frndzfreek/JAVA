211f4e0420c4de3e090b7eef2396b09edab5e204
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-01 16:35:18 +0530

    Create Array.java

---
+++ RECURSIVE/Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void Array(int[] arr, int index) {
+        if (index == a.length) return;  // Base case
+        System.out.print(a[index] + " ");
+        Array(a, index + 1);  // Recursive case
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        Array(arr, 0);
+    }
+}
