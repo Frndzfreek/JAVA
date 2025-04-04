e741dc68b6b5d74e266457681d544754bc28bf9b
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-04 22:41:07 +0530

    Create DataStream.java

---
+++ FILE_HANDLING/DataStream.java
@@ -0,0 +1,16 @@
+import java.io.*;
+//Data streams allow reading and writing primitive data types.
+
+public class DataStreamExample {
+	public static void main(String[] args) {
+		try (DataOutputStream dos = new DataOutputStream(new FileOutputStream("data.dat"))) {
+			dos.writeInt(100);
+			dos.writeDouble(99.99);
+			dos.writeBoolean(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+//Output: A binary file data.dat will be created containing 100, 99.99, and true.
