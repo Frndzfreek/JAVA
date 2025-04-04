b5a23721a323192b38cd36fbfbcb7718a86cbe8b
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-04 22:44:32 +0530

    Create Serialization.java

---
+++ FILE_HANDLING/Serialization.java
@@ -0,0 +1,25 @@
+import java.io.*;
+//Serialization converts an object into a byte stream for storage or transmission.
+
+class Person implements Serializable {
+	private static final long serialVersionUID = 1L;
+	String name;
+	int age;
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
+
+public class SerializationExample {
+	public static void main(String[] args) {
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("person.ser"))) {
+			Person p = new Person("Alice", 30);
+			oos.writeObject(p);
+		} catch (IOException e) {
+			e.printStackTrace();
+    }
+	}
+}
+
+//Output: A file person.ser will be created containing the serialized object.
