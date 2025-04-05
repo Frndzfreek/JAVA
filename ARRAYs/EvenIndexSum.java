571e60efc29ca769c0e84719433e992b8c92d913
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-05 07:58:56 +0530

    Create EvenIndexSum.java

---
+++ ARRAYs/EvenIndexSum.java
@@ -0,0 +1,12 @@
+public class EvenIndexSum {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20, 25, 30};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i += 2) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum of elements at even indexes: " + sum);
+    }
+}
