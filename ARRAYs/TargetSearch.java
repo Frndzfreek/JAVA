a70da98f8cbe0c07c0f4a625e24e70b83b172b1f
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-08 08:46:49 +0530

    Create TargetSearch.java

---
+++ ARRAYs/TargetSearch.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class TargetSearch{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.print("Enter target: ");
+        int target=scan.nextInt();
+        
+        boolean found=false;
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(target==matrix[i][j]){
+                    System.out.println("element was found in index "+"("+i+","+j+")");
+                    found=true;
+                }
+            }
+        }
+        if(found!=true){
+            System.out.println("Element not found");
+        }
+    }
+}
