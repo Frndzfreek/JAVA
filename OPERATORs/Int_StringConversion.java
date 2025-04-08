abfba5efdb85bad07e456c2dd56653e5e1b9c584
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-08 11:27:18 +0530

    Create Int_StringConversion.java

---
+++ OPERATORs/Int_StringConversion.java
@@ -0,0 +1,26 @@
+class Main{
+    public static void main(String[] args){
+        //Int to String
+        
+        int number = 123;
+        String str = String.valueOf(number);
+        System.out.println("Converted: " + str);
+        
+        int number = 123;
+        String str = Integer.toString(number);
+        System.out.println("Converted: " + str);
+        
+        int number = 123;
+        String str = number + ""; // Just add empty string
+        System.out.println("Converted: " + str);
+        
+        //String to int
+        String str = "123";
+        int number = Integer.parseInt(str);
+        System.out.println("Converted: " + number);
+        
+        String str = "123";
+        int number = Integer.parseInt(str);
+        System.out.println("Converted: " + number);
+    }
+}
