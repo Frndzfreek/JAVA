95f75d62b68a1f3eaa5cd39d15f2ff7b96be5e4b
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-08 09:07:15 +0530

    Update IdentityMatrix.java

--- ARRAYs/IdentityMatrix.java
+++ ARRAYs/IdentityMatrix.java
@@ -1,16 +1,25 @@
+import java.util.Scanner;
 class Main{
     public static void main(String[] args){
-        int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
-        String iden="Yes";//or boolean iden=true;
+        Scanner scan=new Scanner(System.in);
+        int row=scan.nextInt();
+        int[][] matrix=new int[row][row];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<row;j++){
+                matrix[i][j]=scan.nextInt();
+            }
+        }
+        
+        boolean iden=true;//or String iden="yes";
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(i==j && matrix[i][j]!=1){
-                    iden="No";
+                    iden=false;
                 }else if(i!=j && matrix[i][j]==1){
-                    iden="No";
+                    iden=false;
                 }
             }
         }
-        System.out.println("is identity matrix: "+iden);
+        System.out.println(iden);
     }
 }
