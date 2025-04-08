ee0c0fd480c42aa42cd5c14046386541a2eb2b75
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-08 05:30:29 +0530

    Create MergingArrays.java

---
+++ ARRAYs/MergingArrays.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Main{
+    public static void main(String[] args){
+        int[] a={1,3,5,7};
+        int[] b={2,4,6,8};
+        int[] merged=new int[a.length+b.length];
+        
+        int i=0,j=0,k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j]){
+                merged[k++]=a[i++];
+            }else{
+                merged[k++]=b[j++];
+            }
+        }
+        while(i<a.length){
+            merged[k++]=a[i++];
+        }
+        while(i<b.length){
+            merged[k++]=b[j++];
+        }
+        System.out.println(Arrays.toString(merged));
+    }
+}
