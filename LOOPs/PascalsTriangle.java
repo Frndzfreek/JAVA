46e52beaf7a195d1326225d4c8eebb7e1ac28e60
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-08 10:49:06 +0530

    Update PascalsTriangle.java

--- LOOPs/PascalsTriangle.java
+++ LOOPs/PascalsTriangle.java
@@ -1,13 +1,20 @@
 package LOOPs;
-
-public class PascalsTriangle {
-    public static void main(String[] args) {
-        int rows = 5;
-        for (int i = 0; i < rows; i++) {
-            int num = 1;
-            for (int j = 0; j <= i; j++) {
-                System.out.print(num + " ");
-                num = num * (i - j) / (j + 1);
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int rows=scan.nextInt();
+        
+        for(int i=0;i<=rows;i++){
+            //Spaces
+            for (int k = 0; k<rows - i; k++) {
+                System.out.print(" ");
+            }
+            
+            int num=1;
+            for(int j=0;j<=i;j++){
+                System.out.print(num+" ");
+                num=num*(i-j)/(j+1);
             }
             System.out.println();
         }
