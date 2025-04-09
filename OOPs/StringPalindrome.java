15054778a2c0c6cd71fca05ce29db065bfd8aecc
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-09 22:11:51 +0530

    Update StringPalindrome.java

--- OOPs/StringPalindrome.java
+++ OOPs/StringPalindrome.java
@@ -17,3 +17,28 @@ class Main{
         }
     }
 }
+//OR
+import java.util.Scanner;
+class Reverser{
+    public String rev(String a){
+        String b=a;
+        String r="";
+        for(int i=b.length()-1;i>=0;i--){
+            r+=b.charAt(i);
+        }
+        return r;
+    }
+}
+class Main{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        String d=scan.nextLine();
+        Reverser obj=new Reverser();
+        String j=obj.rev(d);
+        if(j.equals(d)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not");
+        }
+    }
+}
