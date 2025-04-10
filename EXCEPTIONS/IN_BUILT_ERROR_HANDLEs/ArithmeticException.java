5afdd4e986b1c0d75c6e5b749c72d40f0a892913
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-10 23:15:22 +0530

    Create ArithmeticException.java

---
+++ EXCEPTIONS/IN_BUILT_ERROR_HANDLEs/ArithmeticException.java
@@ -0,0 +1,10 @@
+public class Example1 {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
