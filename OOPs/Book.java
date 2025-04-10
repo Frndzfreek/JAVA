18f05cdd1680c67a7f139099dbb9b615d305806e
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-10 23:05:41 +0530

    Create Book.java

---
+++ OOPs/Book.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Book{
+    String title;
+    String author;
+    double price;
+    Book(String title,String author,double price){
+        this.title=title;
+        this.author=author;
+        this.price=price;
+    }
+    public void display(){
+        System.out.println(title+" "+author+" "+price);
+    }
+}
+class Main{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        String title=scan.nextLine();
+        String author=scan.nextLine();
+        double price=scan.nextDouble();
+        Book book=new Book(title,author,price);
+        book.display();
+    }
+}
