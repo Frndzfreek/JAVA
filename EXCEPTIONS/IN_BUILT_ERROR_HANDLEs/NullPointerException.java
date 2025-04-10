2d0c8f5592faa0424fd8514da897be93b7de8f33
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-10 23:16:37 +0530

    Create NullPointerException.java

---
+++ EXCEPTIONS/IN_BUILT_ERROR_HANDLEs/NullPointerException.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException!");
+        }
+    }
+}
