b0c8011b346e293922c1ad8d727b4f008f8872d8
Madhumitha <madhu.mithaemails@gmail.com>
2025-04-10 23:17:22 +0530

    Create NumberFormatException.java

---
+++ EXCEPTIONS/IN_BUILT_ERROR_HANDLEs/NumberFormatException.java
@@ -0,0 +1,10 @@
+public class Main {
+//Thrown when trying to convert a string to a number and the format is invalid.
+    public static void main(String[] args) {
+        try {
+            int num = Integer.parseInt("abc");
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format!");
+        }
+    }
+}
