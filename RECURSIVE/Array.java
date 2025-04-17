cc2c84015b868a7b61dfc071e1e3e8569173ef4a
frndzfreek <madhu.mithaemails@gmail.com>
2025-04-17 19:31:05 +0530

    RECURSIVE_Files

--- RECURSIVE/Array.java
+++ RECURSIVE/Array.java
@@ -1,12 +1,12 @@
 public class Array {
-    public static void Array (int[] a, int index) {
+    public static void Arrays (int[] a, int index) {
         if (index == a.length) return;  // Base case
         System.out.print(a[index] + " ");
-        Array(a, index + 1);  // Recursive case
+        Arrays(a, index + 1);  // Recursive case
     }
 
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4, 5};
-        Array(a, 0);
+        Arrays(a, 0);
     }
 }
